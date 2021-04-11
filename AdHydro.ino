7d3ed3dc8ab0d9f3339f660fa427b430645c31ca
szarath <szarath@users.noreply.github.com>
2021-04-11 21:41:21 +0200

    first commit

---
+++ AdHydro.ino
@@ -0,0 +1,22 @@
+const int pumpin = A0;
+const int pumpout= 9;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
+
+
+void watertemp() {
+  // put your main code here, to run repeatedly:
+
+
+}
